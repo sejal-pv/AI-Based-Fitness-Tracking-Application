d51e3fab20b8a6703e7c5382d1230f25b2164b1b
Sejal P Vernekar <134220032+sejal-pv@users.noreply.github.com>
2025-11-20 05:53:22 +0000

    Initial project structure setup

---
+++ backend/src/test/java/com/fitnessapp/fitness_backend/FitnessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessapp.fitness_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
